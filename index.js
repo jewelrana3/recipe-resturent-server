d1564bf48c48b2e6a3ff177897e0bbf1d9771712
jewelrana3 <juyelrana7752@gmail.com>
2023-05-03 19:54:19 +0600

    gitignore

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const cors = require('cors')
+const port =process.env.port || 3000
+
+const resturent = require('./data/res.json')
+app.use(cors())
 
 app.get('/',(req,res)=>{
     res.send('hellow bai')
+})
+
+app.get('/resturent',(req,res)=>{
+    res.send(resturent)
 })
 app.listen(port,()=>{
     console.log(`al most right:${port}`)
