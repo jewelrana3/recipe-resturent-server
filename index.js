ea9479ecc8b8bb7dc08a342cdfc375cf36428282
jewelrana3 <juyelrana7752@gmail.com>
2023-05-03 22:04:35 +0600

    food json

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const port =process.env.port || 3000
 
 const resturent = require('./data/res.json')
+const food = require('./data/food.json')
 app.use(cors())
 
 app.get('/',(req,res)=>{
@@ -12,6 +13,16 @@ app.get('/',(req,res)=>{
 
 app.get('/resturent',(req,res)=>{
     res.send(resturent)
+})
+
+app.get('/food',(req,res)=>{
+    res.send(food)
+})
+
+app.get('/food/:id',(req,res)=>{
+    const id = req.params.id;
+    const selected = food.find(n=>n_id === id)
+    res.send(selected)
 })
 app.listen(port,()=>{
     console.log(`al most right:${port}`)
