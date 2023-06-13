6f6f41dc50e300e0fd28f87a890377b572f09153
jewelrana3 <juyelrana7752@gmail.com>
2023-06-13 16:11:54 +0600

    menu

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const port =process.env.port || 3000
+const port = process.env.port || 3000
 
-const resturent = require('./data/res.json')
 const food = require('./data/food.json')
+// const food = require('./data/food.json')
 app.use(cors())
 
 app.get('/',(req,res)=>{
     res.send('hellow bai')
 })
 
-app.get('/resturent',(req,res)=>{
-    res.send(resturent)
+app.get('/food',(req,res)=>{
+    res.send(food)
 })
 
 app.get('/food',(req,res)=>{
@@ -21,20 +21,11 @@ app.get('/food',(req,res)=>{
 
 app.get('/food/:id',(req,res)=>{
     const id = req.params.id;
-    const selected = food.find(n=>n._id === id)
+    console.log(id)
+    const selected = food.find(n=>n.id == id)
     res.send(selected)
 })
-app.get('/resturent/:id',(req,res)=>{
-    const id = parseInt(req.params.id);
-    if(id === 0){
-        res.send(food)
-    }
-    else{
-        const resturentNews = food.filter(n=>parseInt(n.category_id) === id)
-        res.send(resturentNews)
-    }
-   
-})
+
 app.listen(port,()=>{
     console.log(`al most right:${port}`)
 })
